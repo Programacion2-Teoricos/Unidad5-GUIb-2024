09ed5f8f5bd39fb83ae9ae9ec4d2c50454975b0d
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-09-10 20:57:19 -0300

    Update Fecha.java

--- Unidad5_GUIb/src/logica/Fecha.java
+++ Unidad5_GUIb/src/logica/Fecha.java
@@ -6,7 +6,7 @@ public class Fecha {
 	private int mes;
 	private int anio;
 	
-	
+	//constructor específico
 	public Fecha(int dia, int mes, int anio) {
 		super();
 		this.dia = dia;
@@ -14,7 +14,7 @@ public class Fecha {
 		this.anio = anio;
 	}
 	
-	//agregue
+	//constructor por defecto
 	public Fecha() {
 		
 	}
@@ -22,6 +22,38 @@ public class Fecha {
 	@Override
 	public String toString() {
 		return  dia + "/" + mes + "/"+anio;
+	}
+
+	private boolean esBisiesto ()	{
+		
+		return ((anio % 4 == 0) && (anio % 100 != 0) || (anio %400 == 0));		
+	}
+
+	public boolean fechaCorrecta ()	{
+
+		boolean diaCorrecto, mesCorrecto, anyoCorrecto;
+
+		anyoCorrecto = (anio > 0);
+		mesCorrecto = (mes >= 1) && (mes <= 12);
+
+		switch (mes)	{
+			case 2:
+				if (esBisiesto ())	{
+					diaCorrecto = (dia >= 1 && dia <= 29);
+				} else	{
+					diaCorrecto = (dia >= 1 && dia <= 28);
+				}
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				diaCorrecto = (dia >= 1 && dia <= 30);
+				break;
+			default:
+				diaCorrecto = (dia >= 1 && dia <= 31);
+		}
+		return diaCorrecto && mesCorrecto && anyoCorrecto;
 	}
 	
 	
